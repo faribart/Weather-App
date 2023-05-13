f796b50bb8c761836dfb329c47cfa08e8311d943
fariba74 <fariba.rashtbar@yahoo.com>
2023-05-13 16:22:55 +0430

    add api

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-alert("hello")
+function DisplayTemprature(response){
+    console.log(response.data.temperature.current)
+}
+let apiKey="oa5553e83da9c0t3109e1fb49fceca48";
+let query="New York"
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`
+console.log(apiUrl)
+axios.get(apiUrl).then(DisplayTemprature)
