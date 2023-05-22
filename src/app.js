c3e182cb8283722a05c67f9ef152dde4c923723c
fariba74 <fariba.rashtbar@yahoo.com>
2023-05-22 11:44:21 +0330

    current temp

--- src/app.js
+++ src/app.js
@@ -1,5 +1,34 @@
+function FormatData(timestamp){
+    let date=new Date(timestamp);
+    let hour=date.getHours();
+    let minute=date.getMinutes();
+    if(minute<10){
+        minute=`0${minute}`
+    }
+    if(hour<10){
+        hour=`0${hour}`
+    }
+    let days=["Sunday","Monday","Teusday","Wednesday","Thursday","Friday","Saturday"]
+    let day=days[date.getDay()];
+    return`${day} ${hour}:${minute}`
+}
 function DisplayTemprature(response){
-    console.log(response.data.temperature.current)
+    console.log(response.data)
+    let temp=document.querySelector("#temp");
+    temp.innerHTML=Math.round(response.data.temperature.current);
+    let city=document.querySelector("#city");
+    city.innerHTML=response.data.city;
+    let dateTime=document.querySelector("#dateTime");
+    dateTime.innerHTML=FormatData(response.data.time *1000);
+    let description=document.querySelector("#description");
+    description.innerHTML=response.data.condition.description
+    // let pressure=document.querySelector("#pressure");
+    // pressure.innerHTML=response.data.temperature.pressure;
+    let Humidity=document.querySelector("#Humidity");
+    Humidity.innerHTML=response.data.temperature.humidity;
+    let Wind=document.querySelector("#Wind");
+    Wind.innerHTML=Math.round(response.data.wind.speed);
+
 }
 let apiKey="oa5553e83da9c0t3109e1fb49fceca48";
 let query="New York"
